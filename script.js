6150063c122daae720b5fe39ff65f392a3b272e2
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-01-31 12:12:29 +0400

    Done

---
+++ script.js
@@ -0,0 +1,25 @@
+// var a = document.getElementsByClassName("test")
+// console.log(a)
+
+// var b = document.getElementsByTagName("div")
+// console.log(b)
+
+// var c = document.querySelectorAll (".top")
+// console.log(c)
+
+//CALLBACK FUNCTION
+
+// MODERN WAY (ES6 STANDARD)
+//const handleclick = () => {
+    //alert ("hello")
+
+//using arrow method
+//const greet = () => "hello"
+//console.log(greet())
+
+//Passing events by DOM USING CALL BACK ( () =>) FUNCTION - ES6 STANDARD
+
+//document.getElementById("btn").onclick = () => alert("hello")
+
+
+
